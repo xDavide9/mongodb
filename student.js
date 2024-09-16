ba77264d80701a1d6923eac5dceb75708062bb31
xdavide9 <biancodavidetech@gmail.com>
2024-09-16 17:16:35 +0200

    collections and documents

---
+++ student.js
@@ -0,0 +1,14 @@
+student = {
+    "firstName": "Retha",
+    "lastName": "Killeen",
+    "email": "rkilleen0@mysql.com",
+    "gender": "F",
+    "country": "Philippines",
+    "isStudentActive": false,
+    "favouriteSubjects": [
+        "maths",
+        "english",
+        "it"
+    ],
+    "totalSpentInBooks": 0.00
+}
